39e33c81fc291e74a04776ec8924a54314e4f542
George Ruiz <georuiz817@gmail.com>
2019-06-30 03:59:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,11 @@ class Song
   def artist=(artist)
     @artist = artist
   end
+  
+   def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
     
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
