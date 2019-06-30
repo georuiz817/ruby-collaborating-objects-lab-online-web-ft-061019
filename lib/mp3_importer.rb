73898c9aa7c5dc50d259d102288a1f63bf510006
George Ruiz <georuiz817@gmail.com>
2019-06-30 04:14:15 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,21 +1,21 @@
+
+
 class MP3Importer
   attr_accessor :path
-
+  #path = "./spec/fixtures/mp3s"
   def initialize(path)
     @path = path
   end
 
   def files
-    files = []
-    Dir.new(self.path).each do |file|
-      files << file if file.length > 4
+    Dir[@path+"/*"].map do |file|
+      file.split("/").last
     end
-    files
   end
 
   def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
+    files.each do |file|
+      Song.new_by_filename(file)
     end
   end
 end
